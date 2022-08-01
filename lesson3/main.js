7009ba4670772792a0c2a1eb50398a705174a6df
nazar <nazar.yurynets@gmail.com>
2022-08-01 20:15:54 +0300

    les2

--- lesson3/main.js
+++ lesson3/main.js
@@ -2,13 +2,13 @@
 //     Якщо змінна x не дорівнює нулю, виведіть 'Вірно', інакше виведіть 'Невірно'. Перевірте  скрипт при a, що дорівнює 1, 0, -3
 //
 //
-// let x = 1;
-//
-// if (x != 0){
-//     console.log('Вірно');
-// }else {
-//     console.log('Невірно');
-// }
+let a = 0;
+
+if (a !== 0){
+    console.log('Вірно');
+}else {
+    console.log('Невірно');
+}
 //
 // let a = -3;
 //
@@ -22,18 +22,18 @@
 
 // - Дано змінну time яка рівна числу від 0 до 59. Потрібно написати код, який перевірить, до якої четверті години попадає число
 // (в першу, другу, третю или четверту частину години).
-// let time = +prompt("Enter a number from 0 to 59");
-// if (time <= 15){
-//     console.log('the first quarter');
-// }else if(time <= 30){
-//     console.log('the second quarter');
-// }else if (time <= 45) {
-//    console.log( "third quarter");
-// }else if (time <= 60) {
-//      console.log( "fourth quarter");
-// }else {
-//     console.log('wrong time');
-// }
+let time = 54;
+if (time > 0 && time <= 15 ){
+    console.log('the first quarter');
+}else if(time > 15 && time <= 30){
+    console.log('the second quarter');
+}else if (time > 30 && time <= 45) {
+   console.log( "third quarter");
+}else if (time > 45 && time <= 60) {
+     console.log( "fourth quarter");
+}else {
+    console.log('wrong time');
+}
 
 // - У змінній day дано якесь число від 1 до 31. Потрібно визначити, у яку половину(декаду) місяця потрапляє це число (у першу, другу чи третю).
 // let day = +prompt("enter the date from 1 to 31");
